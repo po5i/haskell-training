12fd51ae4b22c0d9e45d6f9ecaac7d3cadfa917e
Carlos Villavicencio <carlos.po5i@gmail.com>
2018-01-31 10:49:35 -0500

    Reviewed by Wellington

--- homework1/app/Main.hs
+++ homework1/app/Main.hs
@@ -3,35 +3,32 @@ module Main where
 import Lib
 
 main :: IO ()
-main = do {
+main = do
   print "Input a credit card number"
-  ; ccNumber <- getLine
-  ; let ccNumberInt = read ccNumber :: Integer
-  ; print "Exercise 1"
-  ; let numberList = toDigits ccNumberInt
-  ; let numberListRev = toDigitsRev ccNumberInt
-  ; print numberList
-  ; print numberListRev
-  ; print "Exercise 2"
-  ; let doubled = doubleEveryOther(toDigits ccNumberInt)
-  ; print doubled
-  ; print "Exercise 3"
-  ; let sumAll = sumDigits doubled
-  ; print sumAll
-  ; print "Exercise 4"
-  ; let result = validate sumAll
-  ; print result
-  ; print "-------------------"
-  ; print "The Towers of Hanoi"
-  ; print "Number of disks:"
-  ; disks <- getLine
-  ; let disksInt = read disks :: Integer
-  ; print "Name of first peg (source)"
-  ; a <- getLine
-  ; print "Name of second peg (target)"
-  ; b <- getLine
-  ; print "Name of third peg (temp)"
-  ; c <- getLine
-  ; let hanoiResult = hanoi disksInt a b c
-  ; print hanoiResult }
-  
+  ccNumber <- getLine
+  let ccNumberInt = read ccNumber :: Integer
+  -- print "Exercise 1"
+  -- let numberList = toDigits ccNumberInt
+  -- let numberListRev = toDigitsRev ccNumberInt
+  -- print numberList
+  -- print numberListRev
+  -- print "Exercise 2"
+  -- let doubled = doubleEveryOther(toDigits ccNumberInt)
+  -- print doubled
+  -- print "Exercise 3"
+  -- let sumAll = sumDigits (doubleEveryOther(toDigits ccNumberInt))
+  -- print sumAll
+  -- print "Exercise 4"
+  print (validate $ sumDigits $ doubleEveryOther $ toDigits ccNumberInt)
+  print "-------------------"
+  print "The Towers of Hanoi"
+  print "Number of disks:"
+  disks <- getLine
+  let disksInt = read disks :: Integer
+  print "Name of first peg (source)"
+  a <- getLine
+  print "Name of second peg (target)"
+  b <- getLine
+  print "Name of third peg (temp)"
+  c <- getLine
+  print (hanoi disksInt a b c)
--- homework1/src/Lib.hs
+++ homework1/src/Lib.hs
@@ -13,18 +13,19 @@ module Lib
 toDigits :: Integer -> [Integer]
 toDigits x
     -- recursion with whole number except the tail, add the tail to result
-    | x > 0 = toDigits(x `div` 10) ++ [x `mod` 10]
+    | x > 0 = toDigits (x `div` 10) ++ [x `mod` 10]
     | otherwise = []
 
 -- toDigitsRev: converts an integer into a inverted list of its digits
 -- Example: toDigitsRev 1234 == [4,3,2,1]
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev x = reverse(toDigits x)
+-- toDigitsRev x = reverse $ toDigits x
+toDigitsRev = reverse . toDigits
 
 -------------
 -- exercise 2
 -- doubleEveryOther:  double every other number beginning from the right
--- using the auxiliary function doubleOtherForward (which does that from 
+-- using the auxiliary function doubleOtherForward (which does that from
 -- left to right)
 -- Example: doubleEveryOther [8,7,6,5] == [16,7,12,5]
 doubleOtherForward :: [Integer] -> [Integer]
@@ -32,31 +33,32 @@ doubleOtherForward (x:y:xs) = x : 2 * y : doubleOtherForward xs
 doubleOtherForward x = x
 
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther x = reverse(doubleOtherForward (reverse x)) -- reverse,reverse?
+-- doubleEveryOther x = reverse $ doubleOtherForward $ reverse x -- reverse,reverse?
+doubleEveryOther = reverse . doubleOtherForward . reverse -- reverse,reverse?
 
 -------------
 -- exercise 3
--- sumDigits: sum up every digit that are part of the list, using the 
+-- sumDigits: sum up every digit that are part of the list, using the
 -- auxiliary function splitDigit which sum up every digit for elements >= 10
 -- Example: sumDigits [16,7,12,5] = 1 + 6 + 7 + 1 + 2 + 5 = 22
 splitDigits :: [Integer] -> [Integer]
 splitDigits [] = []
 splitDigits (x:xs)
     | x >= 10 = [x `div` 10] ++ [x `mod` 10] ++ splitDigits xs
-    | otherwise = [x] ++ splitDigits xs -- Warning: use :
+    | otherwise = x : splitDigits xs -- Warning: use :
 
 sumDigits :: [Integer] -> Integer
-sumDigits x = sum(splitDigits x)
+sumDigits = sum . splitDigits
 
 -------------
 -- exercise 4
 -- validate: validate if a creditar card checksum by calculating the remainder
 -- when the sum is divided by 10. If result = 0, then the number is valid
--- Example: validate 4012888888881881 = True
+-- Example: validate 4012888888881881 = 90 = True
+-- Example: validate 4012888888881882 = 91 = False
 validate :: Integer -> Bool
-validate x
-    | x `mod` 10 /= 0 = False
-    | otherwise = True
+-- validate x = (x `mod` 10) == 0
+validate = (== 0) . flip mod 10
 
 -------------------------------
 -- exercise 5 (Towers of Hanoi)
